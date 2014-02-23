bae994e2b653fc6a7f868671b0817e1b44c9d577
Matt Quinn <sliddizzle@gmail.com>
2014-02-23 20:05:52 +0000

    add missing equal sign

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,7 +14,7 @@ CarrierWave.configure do |config|
     # config.fog_public     = false                                   # optional, defaults to true
     # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 
-    config.storage :fog
+    config.storage = :fog
   
   else
 
